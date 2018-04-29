bcf1b8d763f95e06feb6ecf51918733ee98bf9ac
lukeschoen <dacoolies@gmail.com>
2018-04-30 09:39:14 +1000

    Initial Commit ( basic viewer + git ignore)

---
+++ MeTube/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MeTube3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VideoPanel = new System.Windows.Forms.Panel();
+            this.comboBoxQuality = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // VideoPanel
+            // 
+            this.VideoPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.VideoPanel.Location = new System.Drawing.Point(21, 24);
+            this.VideoPanel.Name = "VideoPanel";
+            this.VideoPanel.Size = new System.Drawing.Size(720, 539);
+            this.VideoPanel.TabIndex = 0;
+            // 
+            // comboBoxQuality
+            // 
+            this.comboBoxQuality.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxQuality.FormattingEnabled = true;
+            this.comboBoxQuality.Location = new System.Drawing.Point(829, 89);
+            this.comboBoxQuality.Name = "comboBoxQuality";
+            this.comboBoxQuality.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxQuality.TabIndex = 1;
+            this.comboBoxQuality.SelectedIndexChanged += new System.EventHandler(this.comboBoxQuality_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 794);
+            this.Controls.Add(this.comboBoxQuality);
+            this.Controls.Add(this.VideoPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel VideoPanel;
+        private System.Windows.Forms.ComboBox comboBoxQuality;
+    }
+}
+
